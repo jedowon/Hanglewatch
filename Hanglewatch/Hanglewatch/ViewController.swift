52b0cdd5d08e73da63e889a3264e0e4ec102bc32
jedowon <jedowon320@naver.com>
2017-01-04 17:32:39 +0900

    Add files via upload

---
+++ Hanglewatch/Hanglewatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hanglewatch
+//
+//  Created by sunrin software10 on 2017. 1. 3..
+//  Copyright © 2017년 sunrin software10. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Hanglewatch/Hanglewatch/time.swift
@@ -0,0 +1,12 @@
+//
+//  time.swift
+//  Hanglewatch
+//
+//  Created by sunrin software10 on 2017. 1. 3..
+//  Copyright © 2017년 sunrin software10. All rights reserved.
+//
+
+import Foundation
+
+let date: NSDate = NSDate()
+print(date)
